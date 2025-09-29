ad234d424294b27b7ae3b3846b477bd430cb5fda
MMphepo <164493503+MMphepo@users.noreply.github.com>
2025-09-29 13:31:01 +0200

    switched to sqlLite

--- BackEnd/MacosBackend/MacosBackend/settings.py
+++ BackEnd/MacosBackend/MacosBackend/settings.py
@@ -80,6 +80,17 @@ WSGI_APPLICATION = "MacosBackend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "macracomplaintsystem",
+#         "USER": "root",     
+#         "PASSWORD": "12345678",
+#         "HOST": "localhost",     
+#         "PORT": "3307",          
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
@@ -90,8 +101,12 @@ DATABASES = {
         "PORT": "3307",          
     }
 }
-
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
