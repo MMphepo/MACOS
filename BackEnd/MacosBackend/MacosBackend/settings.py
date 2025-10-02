0acfcd9d0712024339cfea7c99c1c4475543cfa8
MMphepo <164493503+MMphepo@users.noreply.github.com>
2025-10-02 14:03:11 +0200

    Update Django settings for PostgreSQL and add Supabase configuration

--- BackEnd/MacosBackend/MacosBackend/settings.py
+++ BackEnd/MacosBackend/MacosBackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from supabase import create_client, Client
+
+SUPABASE_URL = "https://wtsojxyaepzqzvbhfxah.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0c29qeHlhZXB6cXp2YmhmeGFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MDE0NDUsImV4cCI6MjA3NDk3NzQ0NX0.RuYfqIeay_BRpMg8cLgIMOM83L_6FPdENPvxVWLTBaA"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,22 +95,33 @@ WSGI_APPLICATION = "MacosBackend.wsgi.application"
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "macracomplaintsystem",
+#         "USER": "root",     
+#         "PASSWORD": "12345678",
+#         "HOST": "localhost",     
+#         "PORT": "3307",          
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "macracomplaintsystem",
-        "USER": "root",     
-        "PASSWORD": "12345678",
-        "HOST": "localhost",     
-        "PORT": "3307",          
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_jmS3VFv4dYDQ',
+        'HOST': 'ep-sparkling-term-ad0khz2i-pooler.c-2.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'channel_binding': 'require'
+        }
     }
 }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
